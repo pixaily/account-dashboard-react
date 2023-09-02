46eb2c191e882213f851479d9f0c99b49bc3f17b
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-09-02 23:45:28 +0300

    Account Dashboard - Add profile tabs component with styles

--- src/components/ui-elements/profile-tabs/ProfileTabs.js
+++ src/components/ui-elements/profile-tabs/ProfileTabs.js
@@ -1,5 +1,16 @@
-const ProfileTabs = () => {
+import { memo } from 'react';
 
+import BaseButton from "../../base-elements/base-button/BaseButton"
+
+import './ProfileTabs.scss';
+
+const ProfileTabs = () => {
+  return (
+    <section class="container profile-tabs">
+      <BaseButton mode="router" className="active" label="My Teams" to="/user-profile"></BaseButton>
+      <BaseButton mode="router" label="About" to="/user-profile"></BaseButton>
+    </section>
+  )
 }
 
-export default ProfileTabs;
+export default memo(ProfileTabs);
