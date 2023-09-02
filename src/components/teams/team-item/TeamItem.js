02bf9f7558a6f49ee06bbb42cff0397d2c6ed591
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-09-02 23:40:11 +0300

    Account Dashboard - Update Team Item with icons and styles

--- src/components/teams/team-item/TeamItem.js
+++ src/components/teams/team-item/TeamItem.js
@@ -1,4 +1,11 @@
+import { ReactComponent, memo } from 'react';
+
 import BaseButton from "../../base-elements/base-button/BaseButton";
+import BaseIcon from "../../base-elements/base-icon/BaseIcon";
+
+import IconBadge from './../../../assets/images/team-placeholder@2x.png';
+import { ReactComponent as IconStadium } from './../../../assets/icons/stadium.svg';
+
 
 const TeamItem = ({
   name,
@@ -6,8 +13,11 @@ const TeamItem = ({
   leagues,
   stadium,
   iconStadium,
-
+  buttonLabel
 }) => {
+
+  badgeSrc = badgeSrc ? badgeSrc : IconBadge;
+
   return (
     <li className="team">
       <div className="team__badge">
@@ -21,14 +31,16 @@ const TeamItem = ({
           <div className="team__name" html={name}></div>
           {/* if stadium */}
           <div className="team__stadium">
-            <base-icon className="icon--stadium" v-html="iconStadium"></base-icon>
+            <BaseIcon className="icon--stadium">
+              <IconStadium />
+            </BaseIcon>
             <span className="team__stadium__name" v-html="stadium" ></span>
           </div>
           {/* end stadium */}
         </div>
       </div>
       <div className="team__action" v-if="showButton">
-        <BaseButton mode="button" label="buttonLabel" className="button button--primary isFollowing ? 'active' : ''"></BaseButton>
+        <BaseButton mode="button" label={buttonLabel} className="button button--primary isFollowing ? 'active' : ''"></BaseButton>
       </div>
     </li>
   )
